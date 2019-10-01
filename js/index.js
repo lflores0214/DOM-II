4237099b70e1677e56bf36f9433940b482c8b1c3
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-10-01 14:47:30 -0600

    started assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const honk = document.querySelector('.logo-heading');
+
+honk.addEventListener('dblclick', () => {
+    const audio = document.querySelector('.horn');
+    audio.play();
+})
+
+const textTransform = document.querySelectorAll('.text-content');
+console.log(textTransform);
+
+textTransform.addEventListener('resize', () => {
+    textTransform.style.color = "red";
+    textTransform.style.transition = "all 0.7s"
+})
